17ab06c5708b3775d12772107b73e718802473e2
John Medilo <medilo06@gmail.com>
2026-01-19 20:37:57 -0800

    0.0.1-Final-ongoing

--- scripts/installation/macOS/install.sh
+++ scripts/installation/macOS/install.sh
@@ -1,2 +1,28 @@
 brew update
-brew install hyperkit
+# brew install hyperkit     # Warning: hyperkit has been deprecated because it is not maintained upstream!. It was disabled on 2025-06-21.
+
+####################################
+# Colima internally replaces:
+# hyperkit
+# docker-machine
+####################################
+brew install colima
+colima start
+
+####################################
+# Install minikube
+####################################
+brew install minikube
+minikube start --driver=docker
+
+
+####################################
+# Install kubectl-cli
+####################################
+
+
+####################################
+# Create basic nginx deployment
+####################################
+kubectl create deployment nginx-depl --image=nginx
+kubectl get replicaset
